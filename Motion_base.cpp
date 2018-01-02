b2f85c00316363cca7acd52caa92276e86878d8f
Julien <34742806+Steins7@users.noreply.github.com>
2018-01-02 13:01:08 +0100

    coords management
    goToCoords function added
    coords system added
    minor changes

--- Motion_base.cpp
+++ Motion_base.cpp
@@ -1,15 +1,14 @@
 // Driver headers
 #include <A4988.h> //stepper up to 1:16
 #include <DRV8825.h> //stepper up to 1:32
-#include <DRV8834.h> //low volateg stepper up to 1:32
+#include <DRV8834.h> //low voltage stepper up to 1:32
 #include <DRV8880.h> //stepper up to 1:16, with current/torque control
-#include <MultiDriver.h>
-#include <SyncDriver.h>
+#include <MultiDriver.h> //multi-stepper control
+#include <SyncDriver.h> //syncronised multi-stepper control
+#include <math.h> //math lib
 
 // Base Roulante header
 #include "Motion_base.hpp"
-
-#define MATH_PI 3.14159265359
 
 // Dimensionnement
 #define RAYON_ROUE 47 //en mm
@@ -19,24 +18,107 @@
 #define MICROSTEPS 1 //manual microstepping set
 
 // Fonctions
-Motion_base::Motion_base(short rightStepperMovementDirectionPinId, short rightStepperPinId, short leftStepperMovementDirectionPinId, short leftStepperPinId){
+
+Motion_base::Motion_base(
+  short rightStepperMovementDirectionPinId, 
+  short rightStepperPinId, 
+  short leftStepperMovementDirectionPinId, 
+  short leftStepperPinId, 
+  short initialXPos, 
+  short initialYPos, 
+  double initialAngle
+  ){
+    
+  xPos=initialXPos;
+  yPos=initialYPos;
+  angle=initialAngle;
   *right = BasicStepperDriver(NBSTEPS, rightStepperMovementDirectionPinId, rightStepperPinId);
   *left = BasicStepperDriver(NBSTEPS, leftStepperMovementDirectionPinId, leftStepperPinId);
   *controller = SyncDriver(*right, *left);
-  /*gestion du microstepping*/
+  
+  /*gestion physique du microstepping*/
+  
   right->begin(RPM,MICROSTEPS);
   left->begin(RPM,MICROSTEPS);
 }
 
-void Motion_base::moveForward(short d){ //distance en mm
-  /*gestion du microstepping*/
-  const short steps = d*NBSTEPS/2*MATH_PI*RAYON_ROUE;
+/*
+ * move the given distance straight, positive for moving forward, negative for moving backward
+ */
+
+void Motion_base::moveStraight(short d){ //distance en mm
+  
+  /*gestion physique du microstepping*/
+  
+  const short steps = d*NBSTEPS/2*M_PI*RAYON_ROUE;
   controller->move(steps,steps); 
 }
 
-void Motion_base::rotate(short rad){
-  /*gestion du microstepping*/
-  const short steps = rad*RAYON_ROBOT*NBSTEPS/2*MATH_PI*RAYON_ROUE;
-  controller->move(steps,steps);
+/*
+ * rotate the given angle, positive to rotate in the trigonometric way, negative to rotate clockwise
+ */
+
+void Motion_base::rotate(double rad){
+  
+  /*gestion physique du microstepping*/
+  
+  const short steps = rad*RAYON_ROBOT*NBSTEPS/2*M_PI*RAYON_ROUE;
+  controller->move(steps,-steps);
   
 }
+
+void Motion_base::goToCoords(short finalXPos, short finalYPos, double finalAngle){
+
+  const short deltaX = finalXPos-xPos;
+  const short deltaY = finalYPos-yPos;
+
+  //gestion de  la position
+  if(deltaX != 0 || deltaY != 0){ // not null movement
+    
+    if(deltaX == 0){ // only y movement
+      rotate(M_PI-angle);
+      moveStraight(deltaY);
+      
+      //update of the Y pos
+      yPos = finalYPos; 
+    }
+    
+    else{
+      
+      if(deltaY == 0){ // only x movement
+        rotate(angle);
+        moveStraight(deltaX);
+        
+        //update of the X pos
+        xPos = finalXPos; 
+      }
+      
+      else{ //movement on both axis
+        
+        const double tmpAngle = atan((deltaY)/(deltaX)); //angle for optimal deplacement
+        rotate(tmpAngle-angle); 
+        
+        //update of the current angle
+        angle = tmpAngle; 
+        
+        moveStraight(sqrt(deltaX*deltaX+deltaY*deltaY));
+        
+        //update of the current position
+        xPos = finalXPos;
+        yPos = finalYPos;
+      }
+      
+    }
+    
+  } 
+
+  const double deltaAngle = finalAngle-angle;
+  
+  //gestion de l'angle
+  if(deltaAngle != 0){
+    rotate(deltaAngle);
+    angle = finalAngle;
+  }
+  
+}
+
--- Motion_base.hpp
+++ Motion_base.hpp
@@ -4,14 +4,23 @@
 class Motion_base {
   
   public : 
-    Motion_base(short rightStepperMovementDirectionPinId, short rightStepperPinId, short leftStepperMovementDirectionPinId, short leftStepperPinId);
-    void moveForward(short d);
-    void rotate(short rad);
+    Motion_base(
+      short rightStepperMovementDirectionPinId, 
+      short rightStepperPinId, 
+      short leftStepperMovementDirectionPinId, 
+      short leftStepperPinId, 
+      short initialXpos, 
+      short initialYPos,
+      double initialAngle
+      );
+    void moveStraight(short d);
+    void rotate(double rad);
+    void goToCoords(short finalXpos, short finalYPos, double finalAngle);
     
   private : 
-    short posx;
-    short posy;
-    short posangle;
+    short xPos;
+    short yPos;
+    double angle;
     BasicStepperDriver *right;
     BasicStepperDriver *left;
     SyncDriver *controller;
