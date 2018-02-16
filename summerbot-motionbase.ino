27b976daf0a1227db3f16e877815720b4d20f7b2
titaniumbaryum <titouan.baillon@gmail.com>
2018-02-16 19:33:45 +0100

    mini pointer fix 3

--- summerbot-motionbase.ino
+++ summerbot-motionbase.ino
@@ -1,36 +1,32 @@
 #include <Arduino.h>
 #include "src/StepperDriver/DRV8825.h"
 #include "MotionBase.h"
-IntervalTimer motionTimer;
-DRV8825* right=new DRV8825(200, 31, 29, 26, 25, 24);// steps per rev,dir pin, step pin, mode pin 0, mode pin 1, mode pin 2
+DRV8825* right=new DRV8825(200, 31, 29, 26, 25, 24);
 DRV8825* left =new DRV8825(200, 32, 30, 26, 25, 24);
-MotionBase mb(left,right,33,63); // left motor, right motor, wheel radius, robot radius, x, y, a
+MotionBase mb(left,right,33,63);
 int r = 0;
 void setup (){
+<<<<<<< HEAD
   //Timer
   motionTimer.begin(motionLoop,1000);// 1kHz (1000 is the period in microS)
   motionTimer.priority(1); //slightly above normal
   
   //Serial
+=======
+>>>>>>> parent of 19af5dd... test sketch conversion to IntervalTimer
   Serial.begin(250000);
-  
-  //Test move
-  delay(1000);
+  delay(5000);
   Serial.println("Start");
-  mb.moveTo(100,100,3.141592/2); //move diagonaly to x=100 y=100 then turn to a=90deg
-  Serial.println(mb.movesString()); //should be 3 moves
+  mb.moveTo(100,100,3.141592/2);
+  Serial.println(mb.movesString());
 }
 
 void loop (){
-  //Debug display
-  /*if(r>1000){
+  /*if(r>100){
     Serial.println(mb.movesString());
     Serial.println(right->getRemainingSteps());
     r=0;
-  }
-  r++;*/
+  }*/
+	mb.update();
+  r++;
 }
-void motionLoop(){
-  mb.update();
-}
-
